feb7121b84da97953da367c03c350be879d89db7
Gil Fink <gil.fink@gmail.com>
2016-09-08 13:41:30 +0300

    Added 3 textboxs for RGB

--- app/common/consts.ts
+++ app/common/consts.ts
@@ -4,3 +4,7 @@ export const DEVICE_ID: string = '0xF7345BF8CCEF';
 export const SERVICE_ID: string = '0xffe5';
 export const CHARACTERISTICS_ID: string = '0xffe9';
 
+export function getColorValue(red, green, blue) {
+  return new Uint8Array([0x56, red, green, blue, 0x00, 0xf0, 0xaa]);
+}
+
--- app/pages/device/device.ts
+++ app/pages/device/device.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
 import {BLE} from 'ionic-native';
-import {SERVICE_ID, CHARACTERISTICS_ID} from '../../common/consts';
+import {getColorValue, SERVICE_ID, CHARACTERISTICS_ID} from '../../common/consts';
 
 @Component({
   templateUrl: 'build/pages/device/device.html'
@@ -10,7 +10,9 @@ export class DevicePage {
   private device;
   private connecting: boolean = false;
   private characteristics = [];
-  private writeStr = '';
+  private redStr = '';
+  private greenStr = '';
+  private blueStr = '';
 
   constructor(private navCtrl: NavController,
               private navParams: NavParams) {
@@ -34,7 +36,7 @@ export class DevicePage {
 
   write(deviceId: string) {
     BLE.connect(deviceId).subscribe((device) => {
-        let writeValue = new Uint8Array([ 0xff, 0x0, 0xff, 0x00, 0xf0, 0xaa ]);
+        let writeValue = getColorValue(this.redStr, this.greenStr, this.blueStr);
 
         device.writeWithoutResponse(deviceId, SERVICE_ID, CHARACTERISTICS_ID , writeValue.buffer).then(() => {
           BLE.disconnect(deviceId);
