2be9a150b0647c448fb9f588ae7a8e6fb9843126
Uri Shaked <uri@salsa4fun.co.il>
2016-09-27 11:09:10 +0100

    Fix splash screen in the browser version

---
+++ config/copy.config.js
@@ -0,0 +1,27 @@
+
+// https://www.npmjs.com/package/fs-extra
+
+module.exports = {
+  include: [
+    {
+      src: 'src/assets/',
+      dest: 'www/assets/'
+    },
+    {
+      src: 'src/index.html',
+      dest: 'www/index.html'
+    },
+    {
+      src: 'src/img',
+      dest: 'www/img'
+    },
+    {
+      src: 'node_modules/ionic-angular/polyfills/polyfills.js',
+      dest: 'www/build/polyfills.js'
+    },
+    {
+      src: 'node_modules/ionicons/dist/fonts/',
+      dest: 'www/assets/fonts/'
+    },
+  ]
+};
