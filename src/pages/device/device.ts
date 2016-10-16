dec84b738fa525043792ab1ff79686c2a278f133
Uri Shaked <uri@salsa4fun.co.il>
2016-10-16 21:40:09 +0300

    feat: read current state from the bulb

--- src/pages/device/device.ts
+++ src/pages/device/device.ts
@@ -1,12 +1,14 @@
-import {Component} from '@angular/core';
-import {NavController, NavParams} from 'ionic-angular';
-import {getColorValue, getWhiteValue, SERVICE_ID, CHARACTERISTIC_ID} from '../../common/consts';
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { getColorValue, getWhiteValue, serviceId, charasteristicId, notificationCharacteristicId,
+  notificationServiceId, readBulbStateCommand } from '../../common/consts';
 
 @Component({
   templateUrl: 'device.html'
 })
 export class DevicePage {
   device: BluetoothDevice;
+  gattServer: BluetoothRemoteGATTServer;
   characteristic: BluetoothRemoteGATTCharacteristic;
   connecting: boolean = false;
   mode: 'rgb' | 'white' = 'rgb';
@@ -25,11 +27,15 @@ export class DevicePage {
   ionViewWillEnter() {
     this.connecting = true;
     this.device.gatt.connect()
-      .then(server => server.getPrimaryService(SERVICE_ID))
-      .then(service => service.getCharacteristic(CHARACTERISTIC_ID))
+      .then(server => {
+        this.gattServer = server;
+        return server.getPrimaryService(serviceId);
+      })
+      .then(service => service.getCharacteristic(charasteristicId))
       .then(characteristic => {
         this.characteristic = characteristic;
         this.connecting = false;
+        this.readBulbState();
       })
       .catch(err => {
         console.error('BLE Connection failed!', err);
@@ -41,7 +47,32 @@ export class DevicePage {
     this.device.gatt.disconnect();
   }
 
-  updateColor() {
+  private onBulbStateReceived(data: Uint8Array) {
+    if (data[0] === 0x66) {
+      this.red = data[6];
+      this.green = data[7];
+      this.blue = data[8];
+      this.warmWhite = data[9];
+      this.mode = (data[3] === 0x4b) ? 'white' : 'rgb';
+    }
+  }
+
+  readBulbState() {
+    this.gattServer.getPrimaryService(notificationServiceId)
+      .then(service => service.getCharacteristic(notificationCharacteristicId))
+      .then(notifyCharacteristic => {
+        notifyCharacteristic.addEventListener('characteristicvaluechanged', event => {
+          this.onBulbStateReceived(new Uint8Array(notifyCharacteristic.value.buffer));
+        });
+        return notifyCharacteristic.startNotifications();
+      })
+      .then(() => {
+        this.characteristic.writeValue(new Uint8Array(readBulbStateCommand));
+      })
+      .catch(err => console.error('failed to listen for bulb notifications', err));
+  }
+
+ updateColor() {
     let writeValue = getColorValue(this.red, this.green, this.blue);
     this.characteristic.writeValue(writeValue);
   }
