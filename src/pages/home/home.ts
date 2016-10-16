00b6e3b960e84a0b64d9083bf9a6383d46c45e95
Uri Shaked <uri@salsa4fun.co.il>
2016-10-16 20:54:49 +0300

    iOS Support

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component, NgZone } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
 import { DevicePage } from '../device/device';
 import { BLE } from 'ionic-native';
 import { SERVICE_ID } from '../../common/consts';
@@ -11,13 +11,17 @@ export class HomePage {
   devices = [];
   isScanning: boolean = false;
 
-  constructor(private navCtrl: NavController, private zone: NgZone) {
+  private isAndroid: boolean;
+
+  constructor(private navCtrl: NavController, private zone: NgZone,
+    platform: Platform) {
+    this.isAndroid = platform.is('android');
   }
 
   startScanning() {
     this.devices.length = 0;
     this.isScanning = true;
-    BLE.enable().then(() => {
+    this.enableBLE().then(() => {
       BLE.startScan([SERVICE_ID.toString(16)]).subscribe(device => {
         this.zone.run(() => {
           this.devices.push(device);
@@ -27,6 +31,15 @@ export class HomePage {
       console.error('Failed to enable BLE', err);
       this.isScanning = false;
     });
+  }
+
+  private enableBLE() {
+    if (this.isAndroid) {
+      return BLE.enable();
+    } else {
+      // BLE.enable doesn't work on iOS
+      return Promise.resolve();
+    }
   }
 
   stopScanning() {
