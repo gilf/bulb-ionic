829b71b5735a8f3244ea19631d560b0bdd990ab9
Uri Shaked <uri@salsa4fun.co.il>
2016-10-16 21:18:08 +0300

    Fix warm white mode

--- src/common/consts.ts
+++ src/common/consts.ts
@@ -2,6 +2,10 @@ export const SERVICE_ID = 0xffe5;
 export const CHARACTERISTIC_ID = 0xffe9;
 export const BULB_COLORS = 'bulbColors';
 
-export function getColorValue(red: number, green: number, blue: number, warmWhite: number) {
-  return new Uint8Array([0x56, red, green, blue, warmWhite, 0xf0, 0xaa]);
+export function getColorValue(red: number, green: number, blue: number) {
+  return new Uint8Array([0x56, red, green, blue, 0, 0xf0, 0xaa]);
+}
+
+export function getWhiteValue(warmWhite: number) {
+  return new Uint8Array([0x56, 0, 0, 0, warmWhite, 0x0f, 0xaa]);
 }
